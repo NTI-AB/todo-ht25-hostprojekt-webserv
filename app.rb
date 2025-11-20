3fd53925a229a4d612d820b9a5f8f08c001342d0
NTI-AB <axel.borjeson@elev.ga.ntig.se>
2025-11-20 15:18:30 +0100

    Fixat css från annat projekt och tagit bort icon cuz ugly

--- app.rb
+++ app.rb
@@ -13,3 +13,20 @@ get '/' do
 end
 
 
+get '/' do
+  query = params[:q]
+  db = SQLite3::Database.new("db/fruits.db")
+ 
+  #gör de det till [{},{},{}]
+  db.results_as_hash = true
+  if query && !query.empty?
+    @data = db.execute("SELECT * FROM fruits WHERE name LIKE ?", "%#{query}%")
+  else
+    @data = db.execute("SELECT * FROM fruits")
+  end
+
+  p @data
+
+  slim(:"index")
+
+end
